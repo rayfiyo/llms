997436f23cde0cfc4ce053f80e0f87275c8d093d
rayfiyo <rayfiyorex@gmail.com>
2024-08-29 21:57:59 +0900

    refactor: 💡 ファイル分割

--- dialogue/main.go
+++ dialogue/main.go
@@ -5,33 +5,35 @@ import (
 	"flag"
 	"fmt"
 	"log"
+
+	"github.com/rayfiyo/llms/dialogue/api"
 )
 
 func main() {
 	var (
-		url = flag.String("str", "http://172.27.167.204:11434/api/generate", "api client URL flag")
-		// url = flag.String("str", "http://172.27.167.204:11434/api/chat", "api client URL flag")
-		model = flag.String("str", "Llama-3-Swallow-70B-Instruct-v0.1-Q8_0", "model name flag")
+		url = flag.String("url", "http://172.27.167.204:11434/api/generate", "api client URL flag")
+		// url = flag.String("url", "http://172.27.167.204:11434/api/chat", "api client URL flag")
+		model = flag.String("model", "Llama-3-Swallow-70B-Instruct-v0.1-Q8_0", "model name flag")
 	)
 
 	flag.Parse()
 	prompt := flag.Arg(0)
 
-	client := NewAPIClient(*url)
-
-	requestBody := GenerateRequest{
-		Model:  *model,
-		Prompt: prompt,
-	}
+	client := api.NewAPIClient(*url)
 
 	/*
-		requestBody := ChatRequest{
-			Model: *model,
-			Messages: []Message{
-				{Role: "user", Content: prompt},
-			},
+		requestBody := GenerateRequest{
+			Model:  *model,
+			Prompt: prompt,
 		}
 	*/
+
+	requestBody := api.ChatRequest{
+		Model: *model,
+		Messages: []api.Message{
+			{Role: "user", Content: prompt},
+		},
+	}
 
 	jsonBody, err := json.Marshal(requestBody)
 	if err != nil {
@@ -46,34 +48,39 @@ func main() {
 	var answer string
 	for _, response := range responses {
 		answer += response.Content
+		fmt.Println(response.Content)
 	}
 
-	for i := 1; ; i++ {
-		fmt.Println("- - - - - -")
-		fmt.Println(i)
-		fmt.Println(answer)
-		fmt.Println("- - - - - -")
+	fmt.Println(answer)
 
-		answer = ""
+	/*
+		for i := 1; ; i++ {
+			fmt.Println("- - - - - -")
+			fmt.Println(i)
+			fmt.Println(answer)
+			fmt.Println("- - - - - -")
 
-		requestBody = GenerateRequest{
-			Model:  *model,
-			Prompt: answer,
+			answer = ""
+
+			requestBody = GenerateRequest{
+				Model:  *model,
+				Prompt: answer,
+			}
+
+			jsonBody, err = json.Marshal(requestBody)
+			if err != nil {
+				log.Fatal(err, i)
+			}
+
+			responses, err = client.Chat(jsonBody)
+			if err != nil {
+				log.Fatal(err, i)
+			}
+
+			var answer string
+			for _, response := range responses {
+				answer += response.Content
+			}
 		}
-
-		jsonBody, err = json.Marshal(requestBody)
-		if err != nil {
-			log.Fatal(err, i)
-		}
-
-		responses, err = client.Chat(jsonBody)
-		if err != nil {
-			log.Fatal(err, i)
-		}
-
-		var answer string
-		for _, response := range responses {
-			answer += response.Content
-		}
-	}
+	*/
 }
