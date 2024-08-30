c14e1f8d2a2759b2a2f7aae9dbf17fcbc9f56387
rayfiyo <rayfiyorex@gmail.com>
2024-08-30 12:06:23 +0900

    feat: 🍻 モデルの分岐オプションとリファクタ（言語設定をconstに）

--- dialogue/main.go
+++ dialogue/main.go
@@ -11,12 +11,16 @@ import (
 	"github.com/rayfiyo/llms/dialogue/models"
 )
 
+const language = "日本語のみで出力すること。"
+
 func main() {
 	mode := flag.String(
 		"mode", "generate", "Mode to use: 'chat' or 'generate'.")
 	model := flag.String(
 		"model", "Llama-3-Swallow-70B-Instruct-v0.1-Q8_0",
-		"Model name.")
+		"Set model name.")
+	model1 := flag.String("model1", "", "Overwrite model name for odd cycles.")
+	model2 := flag.String("model2", "", "Overwrite model name for even cycles.")
 	cyclesLimit := flag.Int("limit", 6, "Limit number of sends cycles.")
 	head := flag.String("head", "", "Head of prompt. Fixed statement.")
 	head1 := flag.String("head1", "", "Head of odd cycle prompt. Fixed statement.")
@@ -24,6 +28,7 @@ func main() {
 	tail := flag.String("tail", "", "Head of prompt. Fixed statement.")
 	tail1 := flag.String("tail1", "", "Head of odd cycle prompt. Fixed statement.")
 	tail2 := flag.String("tail2", "", "Head of even cycle prompt. Fixed statement.")
+	init := flag.String("init", "", "Set prompt as initial question (0th).")
 	flag.Parse()
 	prompt := flag.Arg(0)
 
@@ -33,21 +38,30 @@ func main() {
 	fileName := cmd.GenerateFileName()
 
 	if err := files.Append(fileName, "---\nhead: "+*head+"\nhead1: "+*head1+"\nhead2: "+*head2+
-		"\nprompt: "+prompt+
+		"\nprompt: "+prompt+"\ninit: "+*init+
 		"\ntail: "+*tail+"\ntail1: "+*tail1+"\ntail2: "+*tail2+"\n---\n"); err != nil {
-		log.Fatalf("Error appending to file 1@%d: %v", 0, err)
+		log.Fatalf("Error appending options to file: %v", err)
 	}
 
 	client := api.NewClient("http://172.27.167.204:11434")
 
 	for i := 1; i < *cyclesLimit+1; i++ {
 		// 整形
-		if i%2 != 0 {
+		if *init != "" {
+			i = 0
+			prompt = language + "\n" + *head + "\n" + *init + "\n" + *tail
+		} else if i%2 != 0 {
 			// 1 odd
-			prompt = "日本語で出力すること。\n" + *head + "\n" + *head1 + "\n" + prompt + "\n" + *tail + "\n" + *tail1
+			prompt = language + "\n" + *head + "\n" + *head1 + "\n" + prompt + "\n" + *tail + "\n" + *tail1
+			if *model1 != "" {
+				model = model1
+			}
 		} else {
 			// 2 even
-			prompt = "日本語で出力すること。\n" + *head + "\n" + *head2 + "\n" + prompt + "\n" + *tail + "\n" + *tail2
+			prompt = language + "\n" + *head + "\n" + *head2 + "\n" + prompt + "\n" + *tail + "\n" + *tail2
+			if *model2 != "" {
+				model = model2
+			}
 		}
 
 		switch *mode {
