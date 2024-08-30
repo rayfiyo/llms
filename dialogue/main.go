c67bc2a609d28a8f923ede9964c8bb7e6fefafd0
rayfiyo <rayfiyorex@gmail.com>
2024-08-30 11:45:14 +0900

    feat: 🍻 tail オプションと head と tail の偶奇

--- dialogue/main.go
+++ dialogue/main.go
@@ -18,18 +18,38 @@ func main() {
 		"model", "Llama-3-Swallow-70B-Instruct-v0.1-Q8_0",
 		"Model name.")
 	cyclesLimit := flag.Int("limit", 6, "Limit number of sends cycles.")
-	head := flag.String("head", "", "Prompt head . Fixed statement.")
+	head := flag.String("head", "", "Head of prompt. Fixed statement.")
+	head1 := flag.String("head1", "", "Head of odd cycle prompt. Fixed statement.")
+	head2 := flag.String("head2", "", "Head of even cycle prompt. Fixed statement.")
+	tail := flag.String("tail", "", "Head of prompt. Fixed statement.")
+	tail1 := flag.String("tail1", "", "Head of odd cycle prompt. Fixed statement.")
+	tail2 := flag.String("tail2", "", "Head of even cycle prompt. Fixed statement.")
 	flag.Parse()
 	prompt := flag.Arg(0)
-
-	fileName := cmd.GenerateFileName()
-
-	client := api.NewClient("http://172.27.167.204:11434")
 
 	var content string
 	var err error
 
+	fileName := cmd.GenerateFileName()
+
+	if err := files.Append(fileName, "---\nhead: "+*head+"\nhead1: "+*head1+"\nhead2: "+*head2+
+		"\nprompt: "+prompt+
+		"\ntail: "+*tail+"\ntail1: "+*tail1+"\ntail2: "+*tail2+"\n---\n"); err != nil {
+		log.Fatalf("Error appending to file 1@%d: %v", 0, err)
+	}
+
+	client := api.NewClient("http://172.27.167.204:11434")
+
 	for i := 1; i < *cyclesLimit+1; i++ {
+		// 整形
+		if i%2 != 0 {
+			// 1 odd
+			prompt = "日本語で出力すること。\n" + *head + "\n" + *head1 + "\n" + prompt + "\n" + *tail + "\n" + *tail1
+		} else {
+			// 2 even
+			prompt = "日本語で出力すること。\n" + *head + "\n" + *head2 + "\n" + prompt + "\n" + *tail + "\n" + *tail2
+		}
+
 		switch *mode {
 		case "chat":
 			request := &models.ChatRequest{
@@ -65,11 +85,6 @@ func main() {
 		fmt.Println("- - - - - - - - - - - -")
 		fmt.Printf("%3d:\n%s\n", i, content)
 
-		// 後処理
-		if i%2 != 0 {
-			prompt = "出力言語: 日本語\n" + *head + "\n" + content + "\n何か質問はありますか？"
-		} else {
-			prompt = "出力言語: 日本語\n" + *head + "\n" + content
-		}
+		prompt = content
 	}
 }
